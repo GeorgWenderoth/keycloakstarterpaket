51ee857ae9cf0bcb002ffba03024853ce3298db6
Georg Wenderoth <gwe@adorsys.de>
2022-03-22 12:29:52 +0100

    finish for review

--- keycloakstarterpaket/src/test/java/com/keycloak/starterpaket/controller/KeycloakServiceTest.java
+++ keycloakstarterpaket/src/test/java/com/keycloak/starterpaket/controller/KeycloakServiceTest.java
@@ -4,37 +4,19 @@ package com.keycloak.starterpaket.controller;
 import com.keycloak.starterpaket.requests.AuthAccess;
 import com.keycloak.starterpaket.responses.AuthUrl;
 import com.keycloak.starterpaket.service.KeycloakService;
-import com.mashape.unirest.http.Headers;
-import com.mashape.unirest.http.HttpMethod;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import com.mashape.unirest.request.HttpRequestWithBody;
-import kong.unirest.MockClient;
-
-//import kong.unirest.HttpMethod;
-import lombok.RequiredArgsConstructor;
-import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(SpringRunner.class)
-
-
 public class KeycloakServiceTest {
-
 
     private  KeycloakService keycloakService = new KeycloakService();
 
@@ -44,25 +26,19 @@ public class KeycloakServiceTest {
     @Value("${token-endpoint}")
     private String token_endpoint;
 
-
-
     @Test
     public void generateAuthUrlTest() throws Exception {
 
        var test = keycloakService.generateAuthUrl("http://localhost:3000/*");
         String url = test.getUrl();
 
-       String id = "client_id=" + keycloak_client_id;
         Assert.assertTrue(url.contains("redirect_uri=http://localhost:3000/*"));
         Assert.assertTrue(url.contains("response_type=code"));
-
         Assert.assertTrue(url.contains("code_challenge"));
         Assert.assertTrue(url.contains("code_challenge_method=S256"));
-
         Assert.assertTrue(url.contains("client_id"));
-
-
     }
+
     @Test
     public void test_generateAuthUrlSecondTest() throws Exception {
         var test = keycloakService.generateAuthUrl("zapp");
@@ -94,7 +70,4 @@ public class KeycloakServiceTest {
         var test = keycloakService.findAuthUrl(access);
         assertNull(test);
     }
-
-
-
 }
